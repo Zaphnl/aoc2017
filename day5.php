98e4d52571f5475e0e757596f25ab4568e264178
Peter Bonjernoor <peter@zaph.nl>
2017-12-05 11:56:09 +0100

    Easy peasy

--- day5.php
+++ day5.php
@@ -10,9 +10,6 @@ echo "Part 1: $steps steps\n";
 // Do not forget to reload the input! ;-)
 $input = explode("\n", file_get_contents("./input/day5.txt"));
 $steps = $cursor = 0;
-while ($cursor >= 0 && $cursor < $length && ++$steps) {
-	$delta = $input[$cursor];
-	$input[$cursor] += $delta > 2 ? -1 : 1;
-	$cursor += $delta;
-}
+while ($cursor >= 0 && $cursor < $length && ++$steps)
+	$cursor += $input[$cursor] > 2 ? $input[$cursor]-- :$input[$cursor]++;
 echo "Part 2: $steps steps\n";
