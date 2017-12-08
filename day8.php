bde0180b512bb367cfc93633770bc2c49aabe2ce
Peter Bonjernoor <peter@zaph.nl>
2017-12-08 11:01:55 +0100

    Easy peasy

--- day8.php
+++ day8.php
@@ -2,29 +2,24 @@
 $input = explode("\n", file_get_contents("./input/day8.txt"));
 
 $registers = [];
-$max = 0;
+$max = $result = 0;
 foreach ($input as $idx => $line) {
 	list($reg, $act, $num, $if, $cmp, $op, $val) = explode(' ', $line);
 	if (!isset($registers[$reg]))
 		$registers[$reg] = 0;
 	if (!isset($registers[$cmp]))
 		$registers[$cmp] = 0;
-	$cmp = $registers[$cmp];
-	$result = null;
 	switch ($op) {
-		case "<": $result = $cmp < $val; break;
-		case ">": $result = $cmp > $val; break;
-		case "<=": $result = $cmp <= $val; break;
-		case ">=": $result = $cmp >= $val; break;
-		case "==": $result = $cmp == $val; break;
-		case "!=": $result = $cmp != $val; break;
-		default: echo "Error - unknown: $op\n";
+		case "<": if (!($registers[$cmp] < $val)) continue 2; break;
+		case ">": if (!($registers[$cmp] > $val)) continue 2; break;
+		case "<=": if (!($registers[$cmp] <= $val)) continue 2; break;
+		case ">=": if (!($registers[$cmp] >= $val)) continue 2; break;
+		case "==": if (!($registers[$cmp] == $val)) continue 2; break;
+		case "!=": if (!($registers[$cmp] != $val)) continue 2; break;
 	}
-	if ($result) {
-		$registers[$reg] += $act == 'inc' ? $num : -$num;
-		if ($registers[$reg] > $max)
-			$max = $registers[$reg];
-	}
+	$registers[$reg] += $act == 'inc' ? $num : -$num;
+	if ($registers[$reg] > $max)
+		$max = $registers[$reg];
 }
 asort($registers);
 echo "Part 1: Highest register value at end = ".array_pop($registers)."\n";
